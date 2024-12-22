77971ff7606abbc6be3928959e24d635f7f4dac2
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-12-22 16:24:11 +0800

    add scrape the news

--- app/routers/v1/endpoints.py
+++ app/routers/v1/endpoints.py
@@ -1,11 +1,10 @@
-import os
-import httpx
-import json
+from typing import List
 from fastapi import APIRouter, Query, Security
 from app.auth import get_api_key
 from app.models import SearchResponse
 from fastapi.responses import JSONResponse
-from async_lru import alru_cache
+from app.routers.v1.scrape import Scraper
+from app.routers.v1.search_news import search_news
 
 ROUTE_NAME = "v1"
 
@@ -13,30 +12,6 @@ router = APIRouter(
     prefix=f"/{ROUTE_NAME}",
     tags=[ROUTE_NAME],
 )
-
-URL = "https://google.serper.dev/news"
-
-SERPER_API_KEY = os.environ["SERPER_API_KEY"]
-
-
-@alru_cache(maxsize=1024, ttl=60*60*12)
-async def search_news(q, gl="us", hl="en", num=10, tbs="qdr:d"):
-    payload = json.dumps({
-        "q": q,
-        "gl": gl,
-        "hl": hl,
-        "num": num,
-        "tbs": tbs
-    })
-    headers = {
-        'X-API-KEY': SERPER_API_KEY,
-        'Content-Type': 'application/json'
-    }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(URL, headers=headers, data=payload)
-
-    return response.json()
 
 
 @router.get("/news-search", response_model=SearchResponse)
@@ -50,3 +25,12 @@ async def search(
 ):
     result = await search_news(q, gl, hl, num, tbs)
     return JSONResponse(content=result)
+
+
+scraper = Scraper()
+
+
+@router.post("/scrape/", response_model=List[str])
+async def scrape_content(urls: List[str], api_key: str = Security(get_api_key)):
+    content = await scraper.multi_run(urls=urls)
+    return content
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime, timedelta
 import json
 
 
@@ -6,3 +8,41 @@ def read_json_file(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
+
+
+def get_current_time(relative_time_str):
+    # Define a mapping of time units to timedelta
+    time_units = {
+        'second': 'seconds',
+        'minute': 'minutes',
+        'hour': 'hours',
+        'day': 'days',
+        'month': 'months',
+        'year': 'years'
+    }
+
+    # Use regex to extract the number and the time unit (allowing for plural)
+    match = re.match(r'(\d+)\s+(\w+)(s)?\s+ago', relative_time_str)
+    if not match:
+        raise ValueError("Invalid time format")
+
+    quantity = int(match.group(1))
+    unit = match.group(2)
+
+    if unit.endswith("s"):
+        unit = unit[:-1]
+
+    # Convert the unit to the appropriate timedelta
+    if unit in time_units:
+        if unit == 'month':
+            delta = timedelta(days=30 * quantity)  # Approximate month
+        elif unit == 'year':
+            delta = timedelta(days=365 * quantity)  # Approximate year
+        else:
+            delta = timedelta(**{time_units[unit]: quantity})
+
+        # Calculate the current time
+        current_time = datetime.now() - delta
+        return current_time
+    else:
+        raise ValueError("Unsupported time unit")
